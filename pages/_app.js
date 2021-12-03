93f4a4d81e0bc65037cf987ee64586205c3ed649
Lediane <lediane141@gmail.com>
2021-12-03 16:13:34 -0300

    aula 01

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,52 @@
-import '../styles/globals.css'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { AlurakutStyles } from '../src/lib/AlurakutCommons';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+/* reset css */
+
+* {
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
 }
 
-export default MyApp
+body {
+  font-family: sans-serif;
+  background-color: #D9E6F6;
+}
+
+#__next {
+  display:flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+img{
+  max-width: 100%;
+  height: auto;
+  display: block;
+}
+
+${AlurakutStyles}
+
+`
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
+
+function MyApp({ Component, pageProps }) {
+  return (
+   <>
+      <GlobalStyle />
+
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default MyApp;
